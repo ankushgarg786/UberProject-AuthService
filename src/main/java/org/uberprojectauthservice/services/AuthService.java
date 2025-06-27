36dff664fe010be2858d33b67e6cfb6d8d524db7
Ankush Garg <ankushgarg7123@gmail.com>
2025-06-28 03:32:11 +0530

    Added bcrypt for encryptiong the password

--- src/main/java/org/uberprojectauthservice/services/AuthService.java
+++ src/main/java/org/uberprojectauthservice/services/AuthService.java
@@ -1,5 +1,8 @@
 package org.uberprojectauthservice.services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.uberprojectauthservice.dtos.PassengerDTO;
 import org.uberprojectauthservice.dtos.PassengerSignupRequestDTO;
@@ -8,9 +11,13 @@ import org.uberprojectauthservice.repositories.PassengerRepository;
 
 @Service
 public class AuthService {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private final PassengerRepository passengerRepository;
 
-    public AuthService(PassengerRepository passengerRepository) {
+    public AuthService(BCryptPasswordEncoder bCryptPasswordEncoder, PassengerRepository passengerRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.passengerRepository = passengerRepository;
     }
 
@@ -19,7 +26,7 @@ public class AuthService {
                 .email(passengerSignupRequestDTO.getEmail())
                 .name(passengerSignupRequestDTO.getName())
                 .phoneNumber(passengerSignupRequestDTO.getPhoneNumber())
-                .password(passengerSignupRequestDTO.getPassword()) //ToDO we have to use bcrypt to encrypt the password
+                .password(bCryptPasswordEncoder.encode(passengerSignupRequestDTO.getPassword())) //ToDO we have to use bcrypt to encrypt the password
                 .build();
         Passenger newPassenger=passengerRepository.save(passenger);
 
