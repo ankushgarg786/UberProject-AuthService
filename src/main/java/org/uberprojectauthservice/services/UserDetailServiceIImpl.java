99f63ffce7ce414c63e06545c562e1bc8a7e5981
Ankush Garg <ankushgarg7123@gmail.com>
2025-06-29 04:07:10 +0530

    Added UserDetails and UserDetailServiceImpl given by spring security

---
+++ src/main/java/org/uberprojectauthservice/services/UserDetailServiceIImpl.java
@@ -0,0 +1,35 @@
+package org.uberprojectauthservice.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.uberprojectauthservice.helpers.AuthPassengerDetails;
+import org.uberprojectauthservice.models.Passenger;
+import org.uberprojectauthservice.repositories.PassengerRepository;
+
+import java.util.Optional;
+
+/**
+ * This class is responsible for loading the user in the form of UserDetails object for Auth
+ */
+@Service
+public class UserDetailServiceIImpl implements UserDetailsService {
+
+    private final PassengerRepository passengerRepository;
+
+    public UserDetailServiceIImpl(PassengerRepository passengerRepository) {
+        this.passengerRepository = passengerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<Passenger> passenger=passengerRepository.findPassengerByEmail(email);
+        if(passenger.isPresent()) {
+            return new AuthPassengerDetails(passenger.get());
+        }else{
+            throw new UsernameNotFoundException("Cannot find passenger with email: " + email);
+        }
+    }
+}
