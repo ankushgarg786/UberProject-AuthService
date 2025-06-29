5e2033e366e0431168f5c4addec8495476151335
Ankush Garg <ankushgarg7123@gmail.com>
2025-06-30 01:07:24 +0530

    Fixed validate API issue

--- src/main/java/org/uberprojectauthservice/filters/JwtAuthFilter.java
+++ src/main/java/org/uberprojectauthservice/filters/JwtAuthFilter.java
@@ -5,12 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
@@ -18,24 +17,29 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.uberprojectauthservice.services.JwtService;
+import org.uberprojectauthservice.services.UserDetailServiceImpl;
+
 import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
-    private final RequestMatcher urlMatcher=new AntPathRequestMatcher("api/v1/auth/validate", HttpMethod.GET.name());
+    @Autowired
+    private UserDetailServiceImpl userDetailsService;
 
-    public JwtAuthFilter(JwtService jwtService, @Qualifier("userDetailsService") UserDetailsService userDetailsService) {
+    private final RequestMatcher uriMatcher =
+            new AntPathRequestMatcher("/api/v1/auth/validate", HttpMethod.GET.name());
+
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService) {
         this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token=null;
-        if(request.getCookies()!=null) {
+        String token = null;
+        if(request.getCookies() != null) {
             for(Cookie cookie : request.getCookies()) {
                 if(cookie.getName().equals("JwtToken")) {
                     token = cookie.getValue();
@@ -43,26 +47,26 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             }
         }
 
-        if(token==null) {
-            //user has not provided any jwt token, hence request should not go forward
+        if(token == null) {
+            // user has not provided any jwt token hence request should not go forward
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
-        String email=jwtService.extractEmail(token);
-        if(email!=null) {
-            UserDetails userDetails=userDetailsService.loadUserByUsername(email);
-            if(jwtService.validateToken(token,userDetails.getUsername())){
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails, null,null);
+        String email = jwtService.extractEmail(token);
+        if(email != null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            if(jwtService.validateToken(token, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, null);
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        RequestMatcher matcher=new NegatedRequestMatcher(urlMatcher);
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        RequestMatcher matcher = new NegatedRequestMatcher(uriMatcher);
         return matcher.matches(request);
     }
 }
